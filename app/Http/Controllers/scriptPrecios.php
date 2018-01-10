87f17bdc00f118d460f2e72e5d51028ef24269b6
juferz <juan.isfg@gmail.com>
2018-01-09 22:32:04 -0500

    Se edita los scripts para traer solo el precio de las paginas

--- app/Http/Controllers/scriptPrecios.php
+++ app/Http/Controllers/scriptPrecios.php
@@ -4,69 +4,49 @@ require('simple_html_dom.php');
 
     function precioExito($url){
         $html = new simple_html_dom();
-        $html->load_file($url); //Se carga la URL con la libreria
-        $posts = $html->find('p[class=price offer]'); //Guardamos en Posts el precio que esta en span:price-number
-        foreach($posts as $post) {  //Devuelve un array se recorre con el for para imprimir, como es unico valor, solo imprime 1
-            $resultado = str_replace ( ".0", '', $post);
-
-            //echo $resultado;
-            break;
-        }
+        $html->load_file($url);
+        $post = $html->find('p[class=price offer]', 0)->plaintext;
+        $resultado = str_replace ( ".0", '', $post);
         return $resultado;
       }
 
     function precioJumbo($url){
         $html = new simple_html_dom();
         $html->load_file($url); //Se carga la URL con la libreria
-        $posts = $html->find('strong[class=skuBestPrice]'); //Guardamos en Posts el precio que esta en span:price-number
-        foreach($posts as $post) {  //Devuelve un array se recorre con el for para imprimir, como es unico valor, solo imprime 1
-            $resultado = str_replace ( "$", '', $post);
-            $resultado = str_replace ( ".", '', $resultado);
-            $resultado = str_replace ( ",00", '', $resultado);
-            //echo $resultado;
-            break;
-        }
+        $post = $html->find('strong[class=skuBestPrice]', 0)->plaintext;
+        $resultado = str_replace ( "$", '', $post);
+        $resultado = str_replace ( ".", '', $resultado);
+        $resultado = str_replace ( ",00", '', $resultado);
         return $resultado;
     }
 
     function precioCarulla($url){
         $html = new simple_html_dom();
         $html->load_file($url); //Se carga la URL con la libreria
-        $posts = $html->find('div[class=pdpInfoProductPrice]'); //Guardamos en Posts el precio que esta en span:price-number
-        foreach($posts as $post) {  //Devuelve un array se recorre con el for para imprimir, como es unico valor, solo imprime 1
-            $resultado = str_replace ( "$", '', $post);
-            $resultado = str_replace ( ".", '', $resultado);
-            //echo $resultado;
-            break;
-        }
+        $post = $html->find('div[class=pdpInfoProductPrice]', 0)->plaintext;
+        $resultado = str_replace ( "$", '', $post);
+        $resultado = str_replace ( ".", '', $resultado);
         return $resultado;
     }
 
+
+    //Falta organizar EURO
     function precioEuro($url){
         $html = new simple_html_dom();
-        $html->load_file($url); //Se carga la URL con la libreria
-        $posts = $html->find('span[class=woocommerce-Price-amount amount]'); //Guardamos en Posts el precio que esta en span:price-number
-        foreach($posts as $post) {  //Devuelve un array se recorre con el for para imprimir, como es unico valor, solo imprime 1
-            $resultado = str_replace ( ".", '', $post);   
-            //echo $resultado;
-            break;
-            }
-            return $resultado;
+        $html->load_file($url); 
+        $post = $html->find('p[class=price]', 0)->plaintext;
+        $resultado = str_replace ( "$", '', $post);
+        $resultado = str_replace ( ".", '', $resultado);
+        return $resultado;
         }
+
 
     function precioMakro($url){
         $html = new simple_html_dom();
-        $html->load_file($url); //Se carga la URL con la libreria
-        $posts = $html->find('span[class=price-number]'); //Guardamos en Posts el precio que esta en span:price-number
-        foreach($posts as $post) {  //Devuelve un array se recorre con el for para imprimir, como es unico valor, solo imprime 1
-            $resultado = str_replace ( "$", '', $post);
-            $resultado = str_replace ( ".", '', $resultado);
-            $resultado = str_replace ( ",00", '', $resultado);
-            //echo $resultado;
-            break;
-        }
+        $html->load_file($url);
+        $post = $html->find('span[class=price-number]', 0)->plaintext;
+        $resultado = str_replace ( "$", '', $post);
+        $resultado = str_replace ( ".", '', $resultado);
+        $resultado = str_replace ( ",00", '', $resultado);
         return $resultado;
-    }
-
-
 ?>
